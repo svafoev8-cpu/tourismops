4253086b48d95622e9c11743758940c6f92f247e
Farhod <you@example.com>
2025-09-14 22:46:08 +0300

    chore(auth): remove unused import

--- blueprints/auth/routes.py
+++ blueprints/auth/routes.py
@@ -1,6 +1,5 @@
 from flask import render_template, redirect, url_for, flash, request
 from flask_login import login_user, logout_user, login_required
-from werkzeug.security import check_password_hash
 
 from . import bp
 from models import User
